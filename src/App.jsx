999593aa04b83d4a84e45e9f515cc6eaf54d4481
Amandine <amandine@akki.fr>
2024-05-16 16:15:30 +0200

    refactor: update App class component to App function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {Component, Fragment} from "react";
+import {Fragment, useEffect, useRef, useState} from "react";
 
 // Custom Components
 import Goku from "./components/characters/Goku.jsx";
@@ -9,58 +9,53 @@ import EndOfGameModal from "./components/modals/EndOfGameModal.jsx";
 // Custom CSS
 import './App.css'
 
-class App extends Component {
+function App() {
+    // States
+    const [isEndOfGameState, setEndOfGame] = useState(false);
+    const [gokuLifeState, setGokuLife] = useState(100);
+    const [vegetaLifeState, setVegetaLife] = useState(100);
 
-    constructor(props) {
-        super(props);
+    // References
+    const gokuRef = useRef();
+    const vegetaRef = useRef();
 
-        this.vegetaRef = React.createRef();
-        this.gokuRef = React.createRef();
-
-        this.state = {
-            endOfGame: false,
-            vegetaLife: 100,
-            gokuLife: 100,
+    // Cycle
+    useEffect(() => {
+        if (vegetaLifeState <= 0 || gokuLifeState <= 0) {
+            setEndOfGame(true);
         }
+    }, [vegetaLifeState, gokuLifeState])
+
+    const reduceLife = (componentName, power) => {
+        componentName === 'Goku'
+            ? setVegetaLife(prevState => prevState - power)
+            : setGokuLife(prevState => prevState - power);
     }
 
-    reduceLife = (componentName, power) => {
-        const concernedCharacterLife = componentName === 'Goku' ? 'vegetaLife' : 'gokuLife';
+    const reStart = () => {
+        // gokuRef.current.setState({ hits: 0 });
+        // vegetaRef.current.setState({ hits: 0 });
 
-        this.setState(prevState => ({
-            [concernedCharacterLife]: prevState[concernedCharacterLife] - power,
-            endOfGame: (prevState[concernedCharacterLife] - power) <= 0,
-        }));
+        setEndOfGame(false);
+        setGokuLife(100);
+        setVegetaLife(100);
     }
 
-    reStart = () => {
-        this.gokuRef.current.setState({ hits: 0 });
-        this.vegetaRef.current.setState({ hits: 0 });
+    const showModal = isEndOfGameState && <EndOfGameModal reStartHandler={reStart}/>;
 
-        this.setState({
-            endOfGame: false,
-            vegetaLife: 100,
-            gokuLife: 100,
-        })
-    }
-
-    render() {
-        const showModal = this.state.endOfGame && <EndOfGameModal reStartHandler={this.reStart}/>;
-
-        return (
-            <Fragment>
-                <div className="container text-center">
-                    <h1>Goku vs. Vegeta</h1>
-                    <hr/>
-                    <div className="row">
-                        <Vegeta ref={this.vegetaRef} life={this.state.vegetaLife} reduceHandler={this.reduceLife}/>
-                        <Goku ref={this.gokuRef} life={this.state.gokuLife} reduceHandler={this.reduceLife}/>
-                    </div>
+    return (
+        <Fragment>
+            <div className="container text-center">
+                <h1>Goku vs. Vegeta</h1>
+                <hr/>
+                <div className="row">
+                    <Vegeta ref={vegetaRef} life={vegetaLifeState} reduceHandler={reduceLife}/>
+                    <Goku ref={gokuRef} life={gokuLifeState} reduceHandler={reduceLife}/>
                 </div>
-                {showModal}
-            </Fragment>
-        )
-    }
+            </div>
+            {showModal}
+        </Fragment>
+    )
 }
 
 export default App
