2bd57d8cddbfa00141a5e7cf0693856d374042fb
Amandine <amandine@akki.fr>
2024-05-31 16:38:38 +0200

    refactor(end-of-game): display modal > update to function component

--- src/components/modals/EndOfGameModal.jsx
+++ src/components/modals/EndOfGameModal.jsx
@@ -1,20 +1,17 @@
-import { Component } from "react";
 import ReactDOM from "react-dom";
 
 import GameOverPicture from '../../assets/media/game-over.gif'
 
-class EndOfGameModal extends Component {
-    render() {
-        return ReactDOM.createPortal(
-            <div className="modal">
-                <div>
-                    <p><img src={GameOverPicture} alt="Game over" /></p>
-                    <button onClick={() => this.props.reStartHandler()}>Re-start</button>
-                </div>
-            </div>,
-            document.getElementById('second-root')
-        )
-    }
+const EndOfGameModal = (props) => {
+    return ReactDOM.createPortal(
+        <div className="modal">
+            <div>
+                <p><img src={GameOverPicture} alt="Game over" /></p>
+                <button onClick={() => props.reStartHandler()}>Re-start</button>
+            </div>
+        </div>,
+        document.getElementById('second-root')
+    )
 }
 
 export default EndOfGameModal;
